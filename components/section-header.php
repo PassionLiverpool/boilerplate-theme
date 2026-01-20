4ea9562db3a3171f4f425728bdafb57dfa747516
joy-passion <joy@passion-productions.co.uk>
2026-01-20 14:09:05 +0000

    Header tweaks

--- components/section-header.php
+++ components/section-header.php
@@ -1,4 +1,4 @@
 <!-- Header -->
-<<?php echo esc_attr( $header_style ); ?> class="<?php echo $section_class ?>__header font--<?php echo esc_attr($font_colour) ?> alignment--<?php echo $text_alignment ?>">
+<<?php echo esc_attr( $header_style ); ?> class="<?php echo $section_class ?>__header font--<?php echo esc_attr($font_colour) ?> alignment--<?php echo $header_alignment ?>">
     <?php echo esc_html( $header ); ?>
 </<?php echo esc_attr( $header_style ); ?>>
--- components/section-wysiwyg.php
+++ components/section-wysiwyg.php
@@ -1,3 +1,3 @@
-<div class="<?php echo $section_class ?>__wysiwyg font--<?php echo esc_attr($font_colour) ?> alignment--<?php echo $text_alignment ?>">
+<div class="<?php echo $section_class ?>__wysiwyg font--<?php echo esc_attr($font_colour) ?>">
     <?php echo wp_kses_post( $wysiwyg_text ); ?>
 </div>
